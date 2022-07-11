f8c5ca6440e95a77a5b1295f6ea7603957338f55
mosse692 <40247330+mosse692@users.noreply.github.com>
2022-07-11 20:21:41 +0200

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,8 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '024a72bf2bmsh697f8cf6b694f55p115de9jsndb41c2a933e4'
+            'x-rapidapi-host': '',
+            'x-rapidapi-key': ''
         }
     })
 
